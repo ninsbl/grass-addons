7d92d719fc091a48c7cf5ce366269ed77aa7aad1
ninsbl <stefan.blumentrath@gmx.de>
2024-09-22 01:06:07 +0200

    flags

--- src/raster/r.buildvrt.gdal/r.buildvrt.gdal.py
+++ src/raster/r.buildvrt.gdal/r.buildvrt.gdal.py
@@ -38,6 +38,12 @@
 # % required: no
 # %end
 
+# %option G_OPT_M_DIR
+# % key: vrt_directory
+# % description: Directory to store GDAL VRT files in. Default is: $GISDBASE/$PROJECT/$MAPSET/gdal
+# % required: no
+# %end
+
 # %option G_OPT_R_OUTPUT
 # %end
 
@@ -63,14 +69,16 @@ import sys
 from pathlib import Path
 
 import grass.script as gs
-from grass.pygrass.modules import Module
 
 
 def get_raster_gdalpath(map_name, check_linked=True, gis_env=None):
     """Get get the path to a raster map that can be opened by GDAL
+
     Checks for GDAL source of linked raster data and returns those
-    if not otherwise requested"""
+    if not otherwise requested or the path to the header of
+    raster maps in native GRASS GIS format"""
     if check_linked:
+        # Check GDAL link header
         map_info = gs.find_file(map_name)
         header_path = (
             Path(gis_env["GISDBASE"])
@@ -86,6 +94,8 @@ def get_raster_gdalpath(map_name, check_linked=True, gis_env=None):
             )
             if gdal_path.exists():
                 return str(gdal_path)
+
+        # Check if Path can be read from command history
         gdal_path = Path(
             gs.parse_key_val(
                 gs.parse_command("r.info", flags="e", map=map_name)["comments"]
@@ -96,61 +106,70 @@ def get_raster_gdalpath(map_name, check_linked=True, gis_env=None):
         )
         if gdal_path.exists():
             return str(gdal_path)
+
+    # Get native GRASS GIS format header
     gdal_path = Path(gs.find_file(map_name)["file"].replace("/cell/", "/cellhd/"))
     if gdal_path.exists():
         return gdal_path
+
+    # Fail if file path cannot be determined
     gs.fatal(_("Cannot determine GDAL readable path to raster map {}").format(map_name))
 
 
 def main():
     """run the main workflow"""
 
-    gisenv = gs.gisenv()
-    # Create a GDAL directory to place VRTs in
-    vrt_dir = Path(gisenv["GISDBASE"]).joinpath(
-        gisenv["LOCATION_NAME"], gisenv["MAPSET"], "gdal"
-    )
-
     # Check if GRASS GIS driver is available
-    # if not gdal.GetDriverByName("GRASS"):
-    #     gs.fatal(
-    #         _("GRASS GIS driver missing in GDAL. Please install it.")
-    #     )
+    if not gdal.GetDriverByName("GRASS"):
+        gs.warning(
+            _(
+                "The GRASS GIS driver missing in GDAL."
+                "Creating VRTs for maps in native GRASS GIS format will fail. "
+                "Please install the GDAL-GRASS plugin."
+            )
+        )
 
+    # Get GRASS GIS environment info
+    gisenv = gs.gisenv()
+
+    # Get inputs
     if options["input"]:
         inputs = options["input"].split(",")
     else:
         inputs = Path(options["file"]).read_text(encoding="UTF8").strip().split("\n")
-
-    output = options["output"]
 
     if len(inputs) < 1:
         gs.fatal(_("At least one input map is required".format(inputs[0])))
 
     inputs = [get_raster_gdalpath(raster_map, gis_env=gisenv) for raster_map in inputs]
 
-    # Get GRASS GIS environment info
-    grass_env = dict(gs.gisenv())
+    # Get output
+    output = options["output"]
 
-    # Create directory for vrt files if needed
-    vrt_dir = Path(grass_env["GISDBASE"]).joinpath(
-        grass_env["LOCATION_NAME"], grass_env["MAPSET"], "gdal"
-    )
-    if not vrt_dir.is_dir():
-        vrt_dir.mkdir()
+    # Create a directory to place GDAL VRTs in
+    if options["vrt_directory"]:
+        vrt_dir = Path(options["vrt_directory"])
+    else:
+        vrt_dir = Path(gisenv["GISDBASE"]).joinpath(
+            gisenv["LOCATION_NAME"], gisenv["MAPSET"], "gdal"
+        )
+    vrt_dir.mkdir(exist_ok=True, parents=True)
 
+    # Create GDAL VRT
     vrt_path = str(vrt_dir / f"{output}.vrt")
+    gs.verbose(_("Creating GDAL VRT '{}'.").format(vrt_path))
     gdal.BuildVRT(vrt_path, inputs)
 
-    # Setup import module object
-    Module(
+    # Import (link) GDAL VRT
+    gs.run_command(
         "r.external",
         quiet=True,
         overwrite=gs.overwrite(),
-        flags="roa" if flags["r"] else "moa",
+        flags=f"oa{''.join([key for key, val in flags.items() if val])}",
         input=str(vrt_path),
         output=output,
     )
+    gs.raster_history(output, overwrite=True)
 
 
 if __name__ == "__main__":
